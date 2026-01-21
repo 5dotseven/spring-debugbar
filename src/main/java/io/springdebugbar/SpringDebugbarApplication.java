a813e61b0398a88a143e567b7f3e1694d84b36b4
5dotseven <dotseven57@gmail.com>
2026-01-21 20:41:24 +0900

    chore: initialize spring-debugbar project

---
+++ src/main/java/io/springdebugbar/SpringDebugbarApplication.java
@@ -0,0 +1,13 @@
+package io.springdebugbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDebugbarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDebugbarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/springdebugbar/SpringDebugbarApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springdebugbar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDebugbarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
